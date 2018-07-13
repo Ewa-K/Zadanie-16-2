ee73e81f9ad87ec0bb1570222c5c5b02e0574042
Ewa.K <ewa_1988@yahoo.com>
2018-07-13 16:46:28 +0200

    Change project

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,14 +1,6 @@
 import React from 'react';
 import todo from '../containers/App';
 
-function Title(props) {
-	
-	return (
-		<div>
-			<h1>To Do</h1>
-			<p>Liczba zadań: { todo.length }</p>
-		</div>
-	);	
-}
+const Title = props => <h1> {props.title} </h1>
 
 export default Title;
