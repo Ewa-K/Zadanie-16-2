593efba4e4b8aff20d26a5f96203c8a2d31ad408
Ewa.K <ewa_1988@yahoo.com>
2018-07-18 16:45:16 +0200

    Change project

--- src/containers/App.js
+++ src/containers/App.js
@@ -26,7 +26,7 @@ class App extends React.Component {
     render() {  
 	    return (
 	        <div className={style.TodoApp}>
-	            { props.title }
+	            {  }
 	        </div>
 	    );
 	}
